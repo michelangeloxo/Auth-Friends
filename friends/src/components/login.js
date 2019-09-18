aac665a87e64c3113a575ba7def7e2b686fe84ea
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-09-17 22:33:26 -0700

    commit 1

---
+++ friends/src/components/login.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = (props) => {
+const [creds, setCreds] = useState({username: "", password: ""});
+const handleChange = event => {
+    setCreds({...creds, [event.target.name]: event.target.value});
+};
+
+const handleSubmit = event => {
+    event.preventDefault();
+    axios.post('http://localhost:5000/api/login', creds)
+    .then(res => {console.log(res);})
+    .catch(err => console.log(err.response));
+}
+    return (
+    <form onSubmit={handleSubmit}>
+
+<input type='text' name='username' placeholder='username' onChange={handleChange} value={creds.username}/>
+
+<input type='password' name='password' placeholder='password' onChange={handleChange} value={creds.password}/>
+
+<button type='submit'>Submit</button>
+
+    </form>
+)
+};
+
+export default Login; 
